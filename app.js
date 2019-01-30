edb9799c820ec324598b70d4d3ec31e90f4d1a7b
Thomas Faller <thomasfaller@gmail.com>
2019-01-30 21:10:41 +0000

    added routing

--- app.js
+++ app.js
@@ -3,57 +3,96 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
-// const members = require("./members");
-const { success, error } = require("./functions");
+const { members } = require("./members");
+const { success, error, getIndex, createId } = require("./functions");
 
-const members = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Julie" },
-  { id: 3, name: "Jack" },
-  { id: 4, name: "Paul" }
-];
+let MembersRouter = express.Router();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/v1/members/:id", (req, res) => {
-  res.json(success(members[req.params.id - 1].names));
-});
-
-app.get("/api/v1/members", (req, res) => {
-  if (req.query.max !== undefined && req.query.max > 0) {
-    res.json(success(members.slice(0, req.query.max)));
-  } else if (req.query.max !== undefined) {
-    res.json(error("Wrong max value"));
-  } else {
-    res.json(success(members));
-  }
-});
-
-app.post("/api/v1/members", (req, res) => {
-  if (req.body.name) {
-    let sameName = false;
-    for (let i = 0; i < members.length; i++) {
-      if (members[i].name == req.body.name) {
-        sameName = true;
-        break;
+MembersRouter.route("/:id")
+  // RETRIEVE A SINGLE MEMBER
+  .get((req, res) => {
+    let index = getIndex(members, req.params.id);
+    if (typeof index == "string") {
+      res.json(error(index));
+    } else {
+      res.json(success(members[index]));
+    }
+  })
+  // EDIT A MEMBER
+  .put((req, res) => {
+    let index = getIndex(members, req.params.id);
+    if (typeof index == "string") {
+      res.json(error(index));
+    } else {
+      let same = false;
+      for (let i = 0; i < members.length; i++) {
+        if (
+          req.body.name == members[i].name &&
+          req.params.id != members[i].id
+        ) {
+          same = true;
+          break;
+        }
+      }
+      if (same) {
+        res.json(error("Same name"));
+      } else {
+        members[index].name = req.body.name;
+        res.json(success(true));
       }
     }
-
-    if (sameName) {
-      res.json(error("Member already exists."));
+  })
+  // REMOVE A MEMBER
+  .delete((req, res) => {
+    let index = getIndex(members, req.params.id);
+    if (typeof index == "string") {
+      res.json(error(index));
     } else {
-      let member = {
-        id: members.length + 1,
-        name: req.body.name
-      };
-      members.push(member);
-      res.json(success(member));
+      members.splice(index, 1);
+      res.json(success(members));
     }
-  } else {
-    res.json(error("No name value"));
-  }
-});
+  });
+
+MembersRouter.route("/")
+  // RETRIEVE ALL MEMBERS
+  .get((req, res) => {
+    if (req.query.max !== undefined && req.query.max > 0) {
+      res.json(success(members.slice(0, req.query.max)));
+    } else if (req.query.max !== undefined) {
+      res.json(error("Wrong max value"));
+    } else {
+      res.json(success(members));
+    }
+  })
+  // CREATE A MEMBER
+  .post((req, res) => {
+    if (req.body.name) {
+      let sameName = false;
+      for (let i = 0; i < members.length; i++) {
+        if (members[i].name == req.body.name) {
+          sameName = true;
+          break;
+        }
+      }
+      if (sameName) {
+        res.json(error("Member already exists."));
+      } else {
+        let member = {
+          id: createId(members),
+          name: req.body.name
+        };
+        members.push(member);
+        res.json(success(member));
+      }
+    } else {
+      res.json(error("No name value"));
+    }
+  });
+
+app.use("/api/v1/members", MembersRouter);
 
 app.listen(8080, () => console.log("Started on port 8080"));
--- functions.js
+++ functions.js
@@ -5,3 +5,12 @@ exports.success = result => {
 exports.error = message => {
   return { status: "error", message };
 };
+
+exports.getIndex = (array, id) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id == id) return i;
+  }
+  return "Wrong id";
+};
+
+exports.createId = array => array[array.length - 1].id + 1;
